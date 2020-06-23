37567aa3fb503a6877552cae1251f97f807fe61a
Gabriel Prates <gabsprates@gmail.com>
2020-06-22 22:41:03 -0300

    Updates

---
+++ precache-manifest.e4eb8b280d9ed1d7f99ffeb4ec1dcab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c653d37fe39031233134c7f8de714970",
+    "url": "/pratanna-box/index.html"
+  },
+  {
+    "revision": "f64a9b58b90c2cf72b9c",
+    "url": "/pratanna-box/static/css/main.d775ce09.chunk.css"
+  },
+  {
+    "revision": "4b21aec57ab8295e0c10",
+    "url": "/pratanna-box/static/js/2.7dee3560.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pratanna-box/static/js/2.7dee3560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64a9b58b90c2cf72b9c",
+    "url": "/pratanna-box/static/js/main.3cec0683.chunk.js"
+  },
+  {
+    "revision": "b677e4bdb97812b8b6af",
+    "url": "/pratanna-box/static/js/runtime-main.67f36024.js"
+  },
+  {
+    "revision": "74f58ac792722fced3bf61483e165708",
+    "url": "/pratanna-box/static/media/github.74f58ac7.svg"
+  }
+]);
